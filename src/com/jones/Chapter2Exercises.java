8c29477c53259a2239f43c7c6dbbf94d2ed901dd
Jones Alapat <Jonesa@cybage.com>
2017-03-10 11:58:05 +0530

    added the chapter2 4 question solution

--- src/com/jones/Chapter2Exercises.java
+++ src/com/jones/Chapter2Exercises.java
@@ -2,18 +2,21 @@ package com.jones;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Chapter2Exercises {
 
 	public static void main(String[] args) {
+		System.out.println("Execise 1");
 		calculateLength();
+		System.out.println("Execise 4");
 		exercise4();
 	}
 
 	private static void exercise4() {
 		int[] values = { 1, 4, 9, 16 };
-		/*stream.o*/
-		
+		IntStream.of(values).forEach(System.out::println);
 	}
 
 	private static void calculateLength() {
